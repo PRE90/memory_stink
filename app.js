11f408edd28e16f54ef063356c3f9d525d757d93
PRE90 <erian.pr@icloud.com>
2016-09-28 12:36:05 -0700

    Add basic express requirements, server not yet functional

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-var PORT = 8080;
 var app = express();
 
 app.get('/', function(req,res){
 	res.render('index.html')
 })
-
-
-app.listen(PORT, function(){console.log('Server started on port: '+PORT+'...')})
