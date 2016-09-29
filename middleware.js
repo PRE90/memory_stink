084f6accd1edc420539ebca6b088a80295137ce9
PRE90 <erian.pr@icloud.com>
2016-09-29 12:51:37 -0700

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,10 @@
+
+var middleware = {
+	getUsername: function(req, res, next){
+		console.log('Whats your name?');
+		console.log(req);
+		next();
+	};
+};
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var middleware = require('./middleware.js');
 var app = express();
 var PORT = 8080;
 
-app.get('/', function(req, res){
-	res.send('Hello Express, you hit route /');
-});
+
+app.use(middleware.getUsername)
 
 app.use(express.static(__dirname+'/public'));
 
