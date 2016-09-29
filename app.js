05e23a38fcef692e7a56a7d5001609f2fdc39c46
PRE90 <erian.pr@icloud.com>
2016-09-28 20:58:54 -0700

    Implement basic express server and rearrange file structure accordingly

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var app = express();
+var PORT = 8080;
 
-app.get('/', function(req,res){
-	res.render('index.html')
-})
+app.get('/', function(req, res){
+	res.send('Hello Express, you hit route /');
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(PORT, function(){
+	console.log('Server running...')
+});
